44bf924b8c2713e833a40814eb1fe694f480e35c
zaki9501 <your.email@example.com>
2025-06-16 10:47:25 +0200

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -10,6 +9,7 @@ import TxVisualizer from "./pages/TxVisualizer";
 import BlockVisualizer from "./pages/BlockVisualizer";
 import NotFound from "./pages/NotFound";
 import { PrivyProvider } from "@privy-io/react-auth";
+import { Analytics } from '@vercel/analytics/react';
 
 const queryClient = new QueryClient();
 
@@ -31,6 +31,7 @@ const App = () => (
         </BrowserRouter>
       </PrivyProvider>
     </TooltipProvider>
+    <Analytics />
   </QueryClientProvider>
 );
 
